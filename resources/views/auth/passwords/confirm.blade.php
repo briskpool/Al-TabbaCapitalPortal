7ee6006aa6379995d0e333b56bc6dc15886c4c33
briskpool <muzammil.mykhan@gmail.com>
2025-05-04 13:22:09 +0500

    Update confirm.blade.php

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -7,12 +7,11 @@
             <div class="row justify-content-center h-100 align-items-center">
                 <div class="col-xl-5 col-md-6">
                     <div class="mini-logo text-center my-5">
-                        <a href="{{ env('WEBSITE_URL') }}"><img class="h-75"
-                                src="{{ asset('images/logo.png') }}" alt=""></a>
+                        <a href="{{ env('WEBSITE_URL') }}"><img src="{{ asset('images/logo.png') }}" alt="" height="50px"></a>
                     </div>
                     <div class="auth-form card">
                         <div class="card-header justify-content-center">
-                            <h4 class="card-title">Answer the security question</h4>
+                            <h4 class="card-title login-card-header">Answer the security question</h4>
                         </div>
                         <div class="card-body">
 
