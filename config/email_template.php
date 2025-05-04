e02ac4443905413fdef73a10afaf2d9a23aab929
briskpool <muzammil.mykhan@gmail.com>
2025-05-04 11:55:13 +0500

    update

--- config/email_template.php
+++ config/email_template.php
@@ -1,16 +1,16 @@
 <?php
 return [
-    'logo' => 'http://igglobalandsavings.com/templates/email-logo.png',
+    'logo' => 'https://al-tabbaahackettcapital.com/test/templates/email-logo.png',
     'font_link' => 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,800&display=swap',
-    'website_link' => 'https://igglobalandsavings.com',
-    'website_name' => 'igglobalandsavings.com',
-    'twitter' => 'https://twitter.com/IGGlobalSavings',
-    'linkedin' => 'https://www.linkedin.com/company/ig-global-savings/',
-    'rumble' => 'https://rumble.com/v2fzg18-ig-global-and-savings.html',
-    'instagram' => 'https://www.instagram.com/igglobalsavings/',
+    'website_link' => 'https://al-tabbaahackettcapital.com/test/',
+    'website_name' => 'al-tabbaahackettcapital.com',
+    'twitter' => '#',
+    'linkedin' => '#',
+    'rumble' => '#',
+    'instagram' => '#',
     'location' => '',
-    'trustpilot' => 'https://www.trustpilot.com/review/igglobalandsavings.com?utm_medium=trustbox&utm_source=MicroReviewCount',
-    'primary_color' => '#ef3e39',
+    'trustpilot' => '#',
+    'primary_color' => '#ffae31',
     'footer_text' => 'This email and any attachments to it are confidential and intended solely for the person to whom they are
     addressed. They may contain legally privileged information. If you have
     received this in error, please delete this message and let us know by emailing ' . env('MAIL_FROM_ADDRESS') . '.
